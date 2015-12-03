829910f0356cebb02ce97681e443329baab2b8ad
Feifei HANG <l.daniel.hung@gmail.com>
2015-12-03 21:47:59 +0800

    each module only needs to be instantiated once.

--- require.js
+++ require.js
@@ -26,11 +26,18 @@
     if (module in modules === false) {
       throw new Error(module + ' has yet to be defined.');
     }
-
-    var callback = modules[module];
+    var mod = modules[module];
     var exports = {};
-    callback(exports);
-
+    // Each module only needs to be instantiated once.
+    if (mod.hasBeenCalled === false) {
+      var callback = mod.val;
+      callback(exports);
+      mod.val = exports;
+      mod.hasBeenCalled = true;
+    }
+    else {
+      exports = mod.val;
+    }
     return exports;
   }
 
@@ -43,7 +50,10 @@
       throw new Error('Expect a function for require.add. instead of ' +
                      typeof callback + '.');
     }
-    modules[name] = callback;
+    modules[name] = {
+      val: callback,
+      hasBeenCalled: false
+    };
   }
 
   root.require = require;
