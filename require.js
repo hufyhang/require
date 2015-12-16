f92e69e0871c76601d7d660aa382171df71c9792
Feifei HANG <l.daniel.hung@gmail.com>
2015-12-16 14:44:22 +0800

    use required modules in callbacks

--- require.js
+++ require.js
@@ -19,81 +19,97 @@
 * var sq = require('square').square;
 * sq(99);
 *
+* require('square', function (sq) {
+*   sq.square(1000);
+* });
+*
 */
-(function (root) {
-  'use strict';
-  // backward compatibility check.
-  if (typeof root.define !== 'undefined' ||
-      typeof root.require !== 'undefined') {
-    return false;
+( function(root) {
+'use strict';
+// backward compatibility check.
+if (typeof root.define !== 'undefined' ||
+    typeof root.require !== 'undefined') {
+  return false;
+}
+
+var modules = {};
+function require(moduleList, cb) {
+  if (typeof moduleList !== 'string' && moduleList.constructor !== Array) {
+    throw new Error('Expect a string or an array of string for require(...) instead of ' + typeof moduleList + '.');
+
+  }
+  // convert string-named module to array.
+  if (typeof moduleList === 'string') {
+    moduleList = [moduleList];
   }
 
-  var modules = {};
-  function require(module) {
-    if (typeof module !== 'string') {
-      throw new Error('Expect a string for require(...) instead of ' +
-                      typeof module + '.');
-
-    }
+  var exports = [];
+  moduleList.forEach(function(module, index) {
     if (module in modules === false) {
       throw new Error(module + ' has yet to be defined.');
     }
+    exports[index] = {};
     var mod = modules[module];
-    var exports = {};
     // Each module only needs to be instantiated once.
     if (mod.loaded === false) {
       try {
         var callback = mod.val;
         // Get all deps first.
         var deps = [];
-        mod.deps.forEach(function (dep) {
+        mod.deps.forEach(function(dep) {
           deps.push(require(dep));
         });
         // Push `exports` to the front of `deps`.
-        deps.unshift(exports);
+        deps.unshift(exports[index]);
         callback.apply(window, deps);
-      }
-      catch (error) { // Catch all thrown errors.
+      } catch ( error ) { // Catch all thrown errors.
         console.error(error.message);
         // If error, set `exports` to `null`.
-        exports = null;
+        exports.push(null);
       }
       finally {
         // Finally, assign `exports` to module, and sets `loaded` to true.
-        mod.val = exports;
+        mod.val = exports[index];
         mod.loaded = true;
       }
+    } else {
+      exports[index] = mod.val;
     }
-    else {
-      exports = mod.val;
-    }
-    return exports;
+
+  });
+
+  if (typeof cb !== 'function') {
+    return exports.length === 1 ? exports[0] : exports;
+  } else {
+    cb.apply(window, exports);
+    return;
   }
 
-  function define(name, deps, callback) {
-    if (typeof name !== 'string') {
-      throw new Error('Expect a string. Found ' + typeof name + '.');
-    }
-    if (deps.constructor !== Array && typeof deps !== 'function') {
-      throw new Error('Expect an array or a function. Found ' + typeof deps + '.');
-    }
-    if (name in modules) {
-      console.warn('Redeclaration of ' + name);
-      return;
-    }
-    if (typeof deps === 'function') {
-      callback = deps;
-      deps = [];
-    }
+}
 
-    modules[name] = {
-      val: callback,
-      loaded: false,
-      deps: deps
-    };
+function define(name, deps, callback) {
+  if (typeof name !== 'string') {
+    throw new Error('Expect a string. Found ' + typeof name + '.');
+  }
+  if (deps.constructor !== Array && typeof deps !== 'function') {
+    throw new Error('Expect an array or a function. Found ' + typeof deps + '.');
+  }
+  if (name in modules) {
+    console.warn('Redeclaration of ' + name);
+    return;
+  }
+  if (typeof deps === 'function') {
+    callback = deps;
+    deps = [];
   }
 
-  root.require = require;
-  root.define = define;
-})(window);
+  modules[name] = {
+    val: callback,
+    loaded: false,
+    deps: deps
+  };
+}
 
+root.require = require;
+root.define = define;
+} )(window);
